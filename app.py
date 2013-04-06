ade0c980db81447bdde8d872a24c4186037f9d6a
enlore <n.e.lorenson@gmail.com>
2013-04-06 21:56:32 +0000

    Add bill class

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-app.secret_key = 'a;lskfdjaio;enfas;lknev;soi8evnse'
+app.secret_key = 'a;lskfdjaio;en lol dev key fas;lknev;soi8evnse'
 
 app.debug = True
 
 @app.route('/', methods = ['GET'])
 def index():
     return 'hey'
+
+
 
 if __name__ == '__main__':
     app.run()
---
+++ bill.py
@@ -0,0 +1,8 @@
+class Bill(object):
+    due_amount = None
+    due_date = None
+    late_amount = None
+    late_after_date = None
+    termination_date = None
+    name = None
+    pay_to = None
