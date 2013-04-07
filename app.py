20eebf727f384843fb3e8cf2c820e1a335becb77
enlore <n.e.lorenson@gmail.com>
2013-04-07 11:53:57 +0000

    Fix delete route, add split to ui

--- app.py
+++ app.py
@@ -29,9 +29,19 @@ def teardown_request(exception):
 def index():
     cur = g.db.execute('SELECT * from bills')
 
-    bills = [dict(name=row[0], due_amount=row[1], late_amount=row[2], 
-        due_date=row[3], late_after_date=row[4], termination_date=row[5], 
-        pay_to=row[6], id=row[7]) for row in cur.fetchall()]
+    bills = [
+        dict(
+            name=row[0], 
+            due_amount=row[1], 
+            late_amount=row[2], 
+            due_date=row[3], 
+            late_after_date=row[4], 
+            termination_date=row[5], 
+            pay_to=row[6], 
+            id=row[7]) for row in cur.fetchall()]
+        
+    for bill in bills:
+        bill['split'] = bill['due_amount'] / 4
 
     return render_template('index.html', bills = bills)
 
@@ -51,7 +61,7 @@ def create_bill():
             request.form['due_amount'],
             request.form['late_amount'],
             request.form['late_after_date'],
-            request.form['termination_date']
+            request.form['termination_date'],
         ])
 
     g.db.commit()
@@ -60,7 +70,7 @@ def create_bill():
 
 @app.route('/delete/<int:bill_id>', methods = ['GET'])
 def delete(bill_id):
-    g.db.execute('DELETE from bills where name=?', (bill_id,))
+    g.db.execute('DELETE from bills where id=?', (bill_id,))
     g.db.commit()
 
     return redirect(url_for('index'))
