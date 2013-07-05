a34c70164651d46f0a5ffa834bc2f5783d8ac33f
enlore <n.e.lorenson@gmail.com>
2013-07-05 17:36:31 -0500

    Manually enter requirements into setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,5 @@
 from setuptools import setup
 
-def get_requirements(filename):
-    """ Pull requirements from file in application root"""
-    with open('requirements.txt', 'r') as f:
-        lines = f.readlines()
-        reqs = []
-        for line in lines:
-            reqs.append(line.strip())
-        return reqs
-
 setup(
        name='nerdhaus',
        version='1.0.',
@@ -16,5 +7,15 @@ setup(
        packages=['nerdhaus'],
        include_package_data=True,
        zip_safe=False,
-       install_requires=get_requirements('requirements.txt')
+       install_requires=[
+            'Flask==0.10.1',
+            'Jinja2==2.7',
+            'MarkupSafe==0.18',
+            'Werkzeug==0.9.1',
+            'argparse==1.2.1',
+            'distribute==0.6.34',
+            'gunicorn==17.5',
+            'itsdangerous==0.21',
+            'wsgiref==0.1.2'
+           ]
        )
