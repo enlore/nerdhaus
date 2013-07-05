05cac79e6c460bd7f744bd158fee83fea52ba4c5
enlore <n.e.lorenson@gmail.com>
2013-07-05 16:20:52 -0500

    Pull reqs from reqs.text

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 from setuptools import setup
 
+def get_requirements(filename):
+    """ Pull requirements from file in application root"""
+    with open('requirements.txt', 'r') as f:
+        lines = f.readlines()
+        reqs = []
+        for line in lines:
+            reqs.append(line.strip())
+        return reqs
+
 setup(
        name='nerdhaus',
        version='1.0.',
@@ -7,7 +16,5 @@ setup(
        packages=['nerdhaus'],
        include_package_data=True,
        zip_safe=False,
-       install_requires=[
-           'Flask'
-           ]
-        )
+       install_requires=get_requirements('requirements.txt')
+       )
