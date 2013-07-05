86bdcce9f6b819cdca479f4e06490944783177af
enlore <n.e.lorenson@gmail.com>
2013-07-05 16:07:28 -0500

    Configure for deployment with distribute

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+       name='nerdhaus',
+       version='1.0.',
+       long_description=__doc__,
+       packages=['nerdhaus'],
+       include_package_data=True,
+       zip_safe=False,
+       install_requir=['Flask']
+        )
