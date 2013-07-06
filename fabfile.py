9bca1a5500615b66ed5c40f3f91d1fe153034e7b
enlore <n.e.lorenson@gmail.com>
2013-07-06 14:51:17 -0500

    Move remote files around

--- fabfile.py
+++ fabfile.py
@@ -8,9 +8,10 @@ def pack():
 
 def deploy():
     dist = local('python setup.py --fullname', capture=True).strip()
-    put('dist/%s.tar.gz' % dist, '/tmp/nerdhaus.tar.gz')
-    run('mkdir /tmp/nerdhaus')
-    with cd('/tmp/nerdhaus'):
-        run('tar xzf /tmp/nerdhaus.tar.gz')
-        run('/var/www/nerdhaus/web/env/bin/python setup.py install')
-    run('rm -rf /tmp/nerdhaus /tmp/nerdhaus.tar.gz')
+    put('dist/%s.tar.gz' % dist, '/tmp/%s.tar.gz' % dist)
+    with cd('/tmp'):
+        run('tar xzf /tmp/%s.tar.gz' % dist)
+        with cd('%s' % dist):
+            run('/var/www/nerdha.us/env/bin/python setup.py install')
+
+    run('rm -rf /tmp/%s /tmp/%s.tar.gz' % (dist, dist))
