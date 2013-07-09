1c8b1409135ef493a481ba2bc4936285528ea0b8
enlore <n.e.lorenson@gmail.com>
2013-07-09 14:48:29 -0500

    Create portfolio and home pages

--- nerdhaus/routes.py
+++ nerdhaus/routes.py
@@ -3,7 +3,15 @@ from nerdhaus import app
 
 @app.route('/', methods = ['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('frontend/index.html')
+
+@app.route('/blog/', methods=['GET'])
+def blog_index():
+    return render_template('blog/index.html')
+
+@app.route('/portfolio/', methods=['GET','POST'])
+def portfolio_index():
+    return render_template('portfolio.html')
 
 @app.route('/pages/<page>')
 def show(page):
