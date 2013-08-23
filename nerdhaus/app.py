457f4648b8f9b82cdcdfa157bca2096ed49c1c1c
enlore <n.e.lorenson@gmail.com>
2013-08-22 22:25:53 -0500

    Factory pattern for app creation

--- nerdhaus/app.py
+++ nerdhaus/app.py
@@ -4,27 +4,42 @@ from .blog import blog
 from .extensions import login_manager
 import random
 
-app = Flask(__name__)
+BLUEPRINTS = [blog, frontend]
 
-app.secret_key = 'a;lskfdjaio;enfas;lknev;soi8evnse'
+def create_app(app_name=__name__, config=None, blueprints=[]):
+    app = Flask(app_name)
+    configure_app(app, config)
 
-login_manager.init_app(app)
+    if not blueprints:
+        blueprints = BLUEPRINTS
 
-app.register_blueprint(frontend)
-app.register_blueprint(blog)
+    bootstrap_blueprints(app, blueprints)
+    register_extensions(app)
+    return app
+
+def bootstrap_blueprints(app, blueprints):
+    for bp in blueprints:
+        app.register_blueprint(bp)
+
+def configure_app(app, config):
+    app.secret_key = 'a;lskfdjaio;enfas;lknev;soi8evnse'
+
+def register_extensions(app):
+    login_manager.init_app(app)
+
+    @app.errorhandler(404)
+    def not_found(error):
+        gifs = [
+        'gangnamvator.gif',
+        'hammer_duo.gif',
+        'Hammertime.gif',
+        'mc_hammer.gif',
+        'Mc Hammer Zidane Trick-329529.gif',
+        'Only_6c1fbe_419666.jpg',
+        'pancake_dj.gif',
+        ]
+
+        gif = random.choice(gifs)
+        return render_template('errors/404.html', gif=gif)
 
 
-@app.errorhandler(404)
-def not_found(error):
-    gifs = [
-    'gangnamvator.gif',
-    'hammer_duo.gif',
-    'Hammertime.gif',
-    'mc_hammer.gif',
-    'Mc Hammer Zidane Trick-329529.gif',
-    'Only_6c1fbe_419666.jpg',
-    'pancake_dj.gif',
-    ]
-
-    gif = random.choice(gifs)
-    return render_template('errors/404.html', gif=gif)
